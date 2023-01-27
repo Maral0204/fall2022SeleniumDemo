cebb13b6d2638d4b721d94c662a7a93431a3e9cf
Maral0204 <arni200815@gmail.com>
2023-01-27 18:14:24 +0600

    Create Git clean

---
+++ src/main/java/GitCleanDemo/GitClean.java
@@ -0,0 +1,9 @@
+package GitCleanDemo;
+
+public class GitClean {
+    public static void main(String[] args) {
+        System.out.println("Git clean");
+    }
+}
+
+//fatal: clean.requireForce defaults to true and neither -i, -n, nor -f given; refusing to clean
--- src/main/java/gitTest/GitTest1.java
+++ src/main/java/gitTest/GitTest1.java
@@ -3,5 +3,45 @@ package gitTest;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Git Test 1");
+
+//        Git commands
+
+//git init: Initializes a new Git repository.
+//git clone <repository>: Creates a local copy of a remote repository.
+//git add <file>: Adds a file to the staging area.
+//git commit -m "message": Creates a new commit with the changes in the staging area and adds a message to describe the commit.
+//git status: Shows the status of the working directory and the staging area.
+//git log: Shows the commit history.
+//git diff: Shows the differences between the working directory and the most recent commit.
+//git pull: Fetches changes from a remote repository and merges them into the local repository.
+//git push: Pushes changes from the local repository to a remote repository.
+//git branch: Lists all local branches in the repository.
+//git checkout <branch>: Switches to the specified branch.
+//git merge <branch>: Merges the specified branch into the current branch.
+//git stash: Temporarily saves changes that have not been committed.
+//git reset: Reverts changes to the last commit.
+//git rm <file>: Deletes a file from the repository and stages the deletion.
+//git fetch: Fetches changes from remote repository but do not merge it.
+//git remote: Lists all remote repository associated with the current local repository.
+
+
+//git init: Инициализирует новый репозиторий Git.
+//git clone <репозиторий>: создает локальную копию удаленного репозитория.
+//git add <файл>: добавляет файл в промежуточную область.
+//git commit -m "сообщение": создает новую фиксацию с изменениями в промежуточной области и добавляет сообщение для описания фиксации.
+//статус git: показывает статус рабочего каталога и промежуточной области.
+//журнал git: показывает историю фиксации.
+//git diff: показывает различия между рабочим каталогом и самой последней фиксацией.
+//git pull: извлекает изменения из удаленного репозитория и объединяет их в локальный репозиторий.
+//git push: переносит изменения из локального репозитория в удаленный репозиторий.
+//ветвь git: перечисляет все локальные ветви в репозитории.
+//git checkout <ветвь>: переключается на указанную ветвь.
+//git merge <ветвь>: объединяет указанную ветвь в текущую ветвь.
+//git stash: временно сохраняет изменения, которые не были зафиксированы.
+//git reset: возвращает изменения к последней фиксации.
+//git rm <файл>: удаляет файл из репозитория и выполняет поэтапное удаление.
+//git fetch: извлекает изменения из удаленного репозитория, но не объединяет их.
+//git remote: перечисляет все удаленные репозитории, связанные с текущим локальным репозиторием.
+
     }
 }
